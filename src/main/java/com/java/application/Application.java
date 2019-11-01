a94033890cad9745360f0975c25056db02222cf9
kpkrksc@gmail.com <kpkrksc@gmail.com>
2019-11-01 18:50:06 +0530

    This is my jenkins project and this is first commit

---
+++ src/main/java/com/java/application/Application.java
@@ -0,0 +1,14 @@
+package com.java.application;
+
+public class Application
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome Jenkins");
+	}
+
+	public Integer addTwoNumber(Integer a, Integer b)
+	{
+		return a + b;
+	}
+}
---
+++ src/test/java/com/java/application/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.java.application;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ApplicationTest
+{
+
+	@Test
+	public void addTwoNumberTest()
+	{
+		assertEquals(20, 4 * 5);
+	}
+
+}
